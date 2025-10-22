135d2b4f3996c1806a9d8fa16cd1bf02990b8235
“Heorhi <heorhi44@gmail.com>
2025-10-22 22:50:34 +0200

    Add migration for new property

---
+++ InventoryManagementApp/Migrations/20251022204839_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "InventoryItems",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Inventories",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "InventoryItems");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Inventories");
+        }
+    }
+}
