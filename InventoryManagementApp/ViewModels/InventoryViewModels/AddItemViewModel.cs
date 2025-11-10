d16fed6b8cfd016519dabb7497901f5d6669ba7e
“Heorhi <heorhi44@gmail.com>
2025-11-10 21:50:15 +0100

    update

--- InventoryManagementApp/ViewModels/InventoryViewModels/AddItemViewModel.cs
+++ InventoryManagementApp/ViewModels/InventoryViewModels/AddItemViewModel.cs
@@ -5,19 +5,39 @@
         public int InventoryId { get; set; }
 
         public string? CustomString1Value { get; set; }
+        public string? CustomString1Name { get; set; }
         public string? CustomString2Value { get; set; }
+        public string? CustomString2Name { get; set; }
         public string? CustomString3Value { get; set; }
+        public string? CustomString3Name { get; set; }
+
         public int? CustomInt1Value { get; set; }
+        public string? CustomInt1Name { get; set; }
         public int? CustomInt2Value { get; set; }
+        public string? CustomInt2Name { get; set; }
         public int? CustomInt3Value { get; set; }
+        public string? CustomInt3Name { get; set; }
+
         public bool? CustomBool1Value { get; set; }
+        public string? CustomBool1Name { get; set; }
         public bool? CustomBool2Value { get; set; }
+        public string? CustomBool2Name { get; set; }
         public bool? CustomBool3Value { get; set; }
+        public string? CustomBool3Name { get; set; }
+
         public string? CustomMultiline1Value { get; set; }
+        public string? CustomMultiline1Name { get; set; }
         public string? CustomMultiline2Value { get; set; }
+        public string? CustomMultiline2Name { get; set; }
         public string? CustomMultiline3Value { get; set; }
+        public string? CustomMultiline3Name { get; set; }
+
         public string? CustomLink1Value { get; set; }
+        public string? CustomLink1Name { get; set; }
         public string? CustomLink2Value { get; set; }
+        public string? CustomLink2Name { get; set; }
         public string? CustomLink3Value { get; set; }
+        public string? CustomLink3Name { get; set; }
+
     }
 }
